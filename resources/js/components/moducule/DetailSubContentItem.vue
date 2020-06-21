0cd459f6a9a2e491d3cbc1ef748fb0a050b5cc63
amaguri1505 <kokemomo.sour@gmail.com>
2020-06-22 01:14:02 +0900

    #1 WIP

---
+++ resources/js/components/moducule/DetailSubContentItem.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="detail-sub-content-item">
+        <div class="detail-sub-content-item__group">
+            <div class="detail-sub-content-item__title">
+                {{ title }}
+            </div>
+            <div class="detail-sub-content-item__subtitle">
+                {{ subtitle }}
+            </div>
+        </div>
+        <v-divider></v-divider>
+    </div>
+</template>
+
+<style lang="sass">
+    @import "../../../sass/common/_variable.scss"
+    .detail-sub-content-item
+
+        &__group
+            padding: 16px 16px 8px 16px
+
+        &__title
+            font-size: 1.2rem
+            line-height: 1.2
+            margin-bottom: 4px
+
+        &__subtitle
+
+</style>
+<script>
+    export default {
+        props: ['title', 'subtitle'],
+    }
+
+</script>
--- resources/js/components/pages/Detail.vue
+++ resources/js/components/pages/Detail.vue
@@ -26,15 +26,106 @@
                     ペット情報
                 </div>
                 <div class="detail__sub-content detail__sub-content--pet-info">
-
+                    <detail-sub-content-item
+                        title="種類"
+                        v-bind:subtitle="this.type"
+                    ></detail-sub-content-item>
+                    <detail-sub-content-item
+                        title="物件名"
+                        v-bind:subtitle="this.name"
+                    ></detail-sub-content-item>
+                    <detail-sub-content-item
+                        title="Goodポイント"
+                        v-bind:subtitle="this.good"
+                    ></detail-sub-content-item>
+                    <detail-sub-content-item
+                        title="Badポイント"
+                        v-bind:subtitle="this.bad"
+                    ></detail-sub-content-item>
+                    <detail-sub-content-item
+                        title="ペットの種類"
+                        v-bind:subtitle="this.pet_type"
+                    ></detail-sub-content-item>
+                    <detail-sub-content-item
+                        title="ペット数"
+                        v-bind:subtitle="this.pet_cnt"
+                    ></detail-sub-content-item>
+                    <detail-sub-content-item
+                        title="最寄駅"
+                        v-bind:subtitle="this.nearest_station"
+                    ></detail-sub-content-item>
+                    <detail-sub-content-item
+                        title="所在地"
+                        v-bind:subtitle="this.address"
+                    ></detail-sub-content-item>
+                    <div class="detail__map">
+                        <iframe src="https://www.google.com/maps/d/embed?mid=1AJeFEUctVjLyANcnoHSS0-ZWAx0&hl=ja"
+                                width="400" height="320"></iframe>
+                    </div>
                 </div>
             </div>
             <div class="detail__property-info mt-5">
                 <div class="detail__sub-title detail__sub-title--property-info">
                     建物情報
                 </div>
-                <div class="detail__sub-content detail__sub-content--property-info"></div>
-
+                <div class="detail__sub-content detail__sub-content--property-info">
+                    <detail-sub-content-item
+                        title="ペット飼育"
+                        v-bind:subtitle="this.is_pet"
+                    ></detail-sub-content-item>
+                    <detail-sub-content-item
+                        title="賃料"
+                        v-bind:subtitle="this.cost"
+                    ></detail-sub-content-item>
+                    <detail-sub-content-item
+                        title="管理費"
+                        v-bind:subtitle="this.manage_cost"
+                    ></detail-sub-content-item>
+                    <detail-sub-content-item
+                        title="初期費用"
+                        v-bind:subtitle="this.initial_cost"
+                    ></detail-sub-content-item>
+                    <detail-sub-content-item
+                        title="専有面積"
+                        v-bind:subtitle="this.area"
+                    ></detail-sub-content-item>
+                    <detail-sub-content-item
+                        title="間取り"
+                        v-bind:subtitle="this.floor_plan"
+                    ></detail-sub-content-item>
+                    <detail-sub-content-item
+                        title="階数"
+                        v-bind:subtitle="this.floor"
+                    ></detail-sub-content-item>
+                    <detail-sub-content-item
+                        title="築年月"
+                        v-bind:subtitle="this.age"
+                    ></detail-sub-content-item>
+                    <detail-sub-content-item
+                        title="建物構造"
+                        v-bind:subtitle="this.structure"
+                    ></detail-sub-content-item>
+                    <detail-sub-content-item
+                        title="駐車場・バイク・駐輪場"
+                        v-bind:subtitle="this.park"
+                    ></detail-sub-content-item>
+                    <detail-sub-content-item
+                        title="設備"
+                        v-bind:subtitle="this.facility"
+                    ></detail-sub-content-item>
+                    <detail-sub-content-item
+                        title="掲載費"
+                        v-bind:subtitle="this.start_date"
+                    ></detail-sub-content-item>
+                    <detail-sub-content-item
+                        title="物件ID"
+                        v-bind:subtitle="this.id"
+                    ></detail-sub-content-item>
+                    <detail-sub-content-item
+                        title="取扱不動産会社"
+                        v-bind:subtitle="this.corp"
+                    ></detail-sub-content-item>
+                </div>
             </div>
             <div class="detail__inquiry mt-5">
                 <a class="detail__inquiry-btn" href="mailto:">
@@ -60,6 +151,14 @@
             color: colors(primary)
             font-weight: bold
             font-size: 1.25rem
+
+        &__sub-content-group
+            padding: 16px 16px 8px 16px
+
+        &__sub-content-title
+            font-size: 1.2rem
+            line-height: 1.2
+            margin-bottom: 2px
 
         &__catch
             background-color: colors(primary)
@@ -113,11 +212,39 @@
 </style>
 <script>
     import DetailThumbnail from "../moducule/DetailThumbnail";
+    import DetailSubContentItem from "../moducule/DetailSubContentItem";
 
     export default {
         components: {
             DetailThumbnail,
+            DetailSubContentItem,
+        },
+        data() {
+            return {
+                house_property: [],
+                type: "賃貸",
+                name: "サンプル物件",
+                good: "良い点",
+                bad: "悪い点",
+                pet_type: "猫",
+                pet_cnt: "1",
+                nearest_station: "サンプル駅",
+                address: "東京都目黒区上目黒",
+                is_pet: "飼育可能",
+                cost: "100,000円",
+                manage_cost: "10,000円",
+                initial_cost: "敷金0ヶ月 / 礼金0ヶ月 / 保証金0ヶ月",
+                area: "41.77㎡",
+                floor_plan: "1LDK",
+                floor: "3階建",
+                age: "1990年12月",
+                structure: "鉄骨鉄筋コンクリート",
+                park: "有り *お問い合わせ",
+                facility: "鍵交換必須",
+                start_date: "2020/06/25",
+                id: "1",
+                corp: "株式会社marumaru店",
+            }
         }
-
     }
 </script>
