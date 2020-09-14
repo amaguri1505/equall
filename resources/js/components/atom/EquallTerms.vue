aa81c3235d3b171a335c7809209cccdbfd97f6c2
amaguri1505 <kokemomo.sour@gmail.com>
2020-09-14 19:56:26 +0900

    #13 利用規約ダイアログについて

--- resources/js/components/atom/EquallTerms.vue
+++ resources/js/components/atom/EquallTerms.vue
@@ -23,7 +23,7 @@
                 </v-card-title>
 
                 <v-card-text>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    <terms></terms>
                 </v-card-text>
 
                 <v-divider></v-divider>
@@ -48,7 +48,12 @@
             text-align: center
 </style>
 <script>
+    import Terms from "../organism/Terms";
+
     export default {
+        components: {
+            Terms,
+        },
         data () {
             return {
                 dialog: false,
