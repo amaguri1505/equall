169d98a7c295e7051ac84cc00693b14924ae0cdd
amaguri1505 <kokemomo.sour@gmail.com>
2020-09-24 14:43:33 +0900

    #54

--- resources/js/components/includes/Header.vue
+++ resources/js/components/includes/Header.vue
@@ -44,16 +44,6 @@
                             お気に入り
                         </v-list-item-title>
                     </v-list-item>
-                    <v-list-item to="/real-estates-for-cat">
-                        <v-list-item-title>
-                            ねこちゃん用物件
-                        </v-list-item-title>
-                    </v-list-item>
-                    <v-list-item to="/real-estates-for-dog">
-                        <v-list-item-title>
-                            わんちゃん用物件
-                        </v-list-item-title>
-                    </v-list-item>
                     <v-list-item href="https://media.equall.jp" target="_blank">
                         <v-list-item-title>
                             暮らし情報
